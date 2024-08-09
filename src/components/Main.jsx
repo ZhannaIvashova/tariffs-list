18c2c2608c22da990e42c0cec83ddd597a45450a
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-08-09 07:18:36 +0300

    feat: add active-card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,38 +10,34 @@ function Main() {
       price: "300 руб/мес",
       speed: "до 10 Мбит/сек",
       info: "Объем включенного трафика не ограничен",
-      color: "#00BFFF",
     },
     {
       type: "Безлимитный 450",
       price: "450 руб/мес",
       speed: "до 50 Мбит/сек",
       info: "Объем включенного трафика не ограничен",
-      colorType: "#48D1CC",
-      colorPrice: "#40E0D0",
     },
     {
       type: "Безлимитный 550",
       price: "550 руб/мес",
       speed: "до 100 Мбит/сек",
       info: "Объем включенного трафика не ограничен",
-      colorType: "#FF6347",
-      colorPrice: "#FF7F50",
     },
     {
       type: "Безлимитный 1000",
       price: "1000 руб/мес",
       speed: "до 200 Мбит/сек",
       info: "Объем включенного трафика не ограничен",
-      colorType: "#000000",
-      colorPrice: "#474646",
     }
   ];
 
   return (
     <main className="main">
       {data.map((tarif, index) =>
-        <Tarif tarif={tarif} index={index}/>
+        <Tarif 
+        tarif={tarif}
+        isActive={tarif.type === 'Безлимитный 550'}
+        />
       )}
     </main>  
   )
--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -1,23 +1,23 @@
 import './tarif.css';
-import colorType300 from'./colorType300.module.css';
-import colorType450 from './colorType450.module.css';
-import colorType550 from './colorType550.module.css';
-import colorType1000 from './colorType1000.module.css';
+import colorTarif300 from'./colorTarif300.module.css';
+import colorTarif450 from './colorTarif450.module.css';
+import colorTarif550 from './colorTarif550.module.css';
+import colorTarif1000 from './colorTarif1000.module.css';
 
-function Tarif({ tarif }) {
+function Tarif({ tarif, isActive }) {
   
   const styleMap = {
-    'Безлимитный 300': colorType300,
-    'Безлимитный 450': colorType450,
-    'Безлимитный 550': colorType550,
-    'Безлимитный 1000': colorType1000
+    'Безлимитный 300': colorTarif300,
+    'Безлимитный 450': colorTarif450,
+    'Безлимитный 550': colorTarif550,
+    'Безлимитный 1000': colorTarif1000
   };
 
   const colorStyle = styleMap[tarif.type]
 
   return (
-    <div className="card">
-      <div className={`card__type ${colorStyle['color-type']}`}>{tarif.type}</div>
+    <div className={`card ${isActive ? 'card__active' : null}`}>
+      <div className={`card__type ${colorStyle['color-type']} ${isActive ? 'card__type-active' : null}`}>{tarif.type}</div>
       <div className={`card__price ${colorStyle['color-price']}`}>{tarif.price}</div>
       <div className="card__speed">{tarif.speed}</div>
       <div className="card__info">{tarif.info}</div>
